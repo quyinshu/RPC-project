630cc7f5bd5a3eba525faf38519e6702a2f0ed78
yiqu0415 <yiqu0415@uni.sydney.edu.au>
2025-03-05 16:30:43 +0800

    add README.md

---
+++ qys.rpc/src/main/java/Remote/TestRemote.java
@@ -0,0 +1,9 @@
+package Remote;
+
+
+import model.Response;
+import model.User;
+
+public interface TestRemote {
+	public Response testUser(User user);
+}
---
+++ qys.rpc/src/main/java/Remote/UserRemoteImpl.java
@@ -0,0 +1,33 @@
+package Remote;
+
+import java.util.List;
+
+//import javax.annotation.Resource;
+import jakarta.annotation.Resource; // 替换javax为jakarta
+
+import annotation.Remote;
+import model.Response;
+import model.User;
+import service.UserService;
+import util.ResponseUtil;
+
+@Remote
+public class UserRemoteImpl implements UserRemote{
+	
+	@Resource
+	private UserService service;
+	
+	public Response saveUser(User user){
+		service.saveUSer(user);
+		Response response = ResponseUtil.createSuccessResponse(user);
+		
+		return response;
+	}
+	
+	public Response saveUsers(List<User> userlist){
+		service.saveUSerList(userlist);
+		Response response = ResponseUtil.createSuccessResponse(userlist);
+		
+		return response;
+	}
+}
